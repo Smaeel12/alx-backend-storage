7b39ac574c2e1fa5949ff16775ebd6a86b99a852
Smaeel12 <ismailboubkri2002@gmail.com>
2024-10-17 12:47:17 +0000

    Correction of "2. Best band ever!"

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,8 +1,4 @@
 -- script that ranks country origins of bands, ordered by the number of (non-unique) fans
-
--- index on the 'origin' column to speed up grouping and ordering
-CREATE INDEX idx_origin ON metal_bands (origin);
-
 -- Sum the number of fans per country (grouped by origin)
 SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
